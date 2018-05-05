cc621950e2c2cc365791a05527f05d77d0317977
DESKTOP-SB28LRS\Nika <nikachikviladze93@gmail.com>
2018-05-05 23:12:32 +0400

    create project

---
+++ resources/views/list.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ajax</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+	<br>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-lg-offset-3">
+				<div class="panel panel-default">
+				  <div class="panel-heading">
+				    <h3 class="panel-title">Panel title <a href="#" type="button"  data-toggle="modal" data-target="#myModal" class="pull-right"><i class="fa fa-plus "></i></a></h3>
+				  </div>
+				  <div class="panel-body">
+				    <ul class="list-group">
+				      <li class="list-group-item">Cras justo odio</li>
+				      <li class="list-group-item">Dapibus ac facilisis in</li>
+				      <li class="list-group-item">Morbi leo risus</li>
+				      <li class="list-group-item">Porta ac consectetur ac</li>
+				      <li class="list-group-item">Vestibulum at eros</li>
+				    </ul>
+
+				  </div>
+				  
+				</div>
+				
+				<div class="modal fade" id="myModal" tabindex="-1" role="dialog">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title">Modal title</h4>
+				      </div>
+				      <div class="modal-body">
+				        <p>One fine body&hellip;</p>
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        <button type="button" class="btn btn-primary">Save changes</button>
+				      </div>
+				    </div><!-- /.modal-content -->
+				  </div><!-- /.modal-dialog -->
+				</div><!-- /.modal -->
+
+			</div>
+		</div>
+	</div>
+	
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	
+</body>
+</html>
